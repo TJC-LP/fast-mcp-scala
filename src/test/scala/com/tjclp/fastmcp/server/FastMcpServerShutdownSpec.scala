88d7f627cf10f69d62d24a77ca129e8c1b4b13ab
Richie Caputo <rcaputo3@tjclp.com>
2025-04-24 16:29:07 -0400

    Create FastMcpServerShutdownSpec.scala

---
+++ src/test/scala/com/tjclp/fastmcp/server/FastMcpServerShutdownSpec.scala
@@ -0,0 +1,36 @@
+package com.tjclp.fastmcp.server
+
+import java.util.concurrent.atomic.AtomicBoolean
+import zio.*
+import zio.test.*
+
+object FastMcpServerShutdownSpec extends ZIOSpecDefault {
+
+  private final class MockCloseableServer(flag: AtomicBoolean) extends AutoCloseable {
+    override def close(): Unit = flag.set(true)
+  }
+
+  def spec =
+    suite("FastMcpServer graceful-shutdown")(
+      test("runStdio closes the underlying server when interrupted") {
+        for {
+          closedFlag <- ZIO.succeed(new AtomicBoolean(false))
+
+          server <- ZIO.succeed(
+            new FastMcpServer() {
+              override def runStdio() =
+                ZIO.scoped {
+                  ZIO.acquireRelease(
+                    ZIO.succeed(new MockCloseableServer(closedFlag))
+                  )(srv => ZIO.succeed(srv.close())) *> ZIO.never
+                }
+            }
+          )
+
+          fiber <- server.runStdio().fork
+          _ <- TestClock.adjust(10.millis)
+          _ <- fiber.interrupt
+        } yield assertTrue(closedFlag.get())
+      }
+    )
+}
